8246d1439df61d932dc0eb8d5ff6123d55110cc1
Jeffrin G.V <jef1619@gmail.com>
2024-08-05 16:26:41 +0530

    changed

--- components/Form.js
+++ components/Form.js
@@ -38,13 +38,13 @@ const Form = () => {
 
                             </button>
                             <h1 className="absolute top-7 sm:top-10 text-black pl-5 josefin text-xl sm:text-2xl">Looking For &#44;</h1>
-                            <a href="" className="bg-yellow-400 text-black py-2 sm:py-4 px-3 sm:px-6 text-center flex rounded-lg items-center justify-between shadow-md josefin text-lg sm:text-xl group ease duration-100 active:bg-black"><span className="group-active:text-yellow-400">Guidance For School / College Students</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 group-active:text-yellow-400">
+                            <a href="" className="bg-yellow-400 text-black py-2 sm:py-4 px-3 sm:px-6 text-center flex rounded-lg items-center justify-between shadow-md josefin text-lg sm:text-xl group ease duration-100 active:bg-black"><span className="group-active:text-yellow-400">Guidance For School &#47; College Students</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 group-active:text-yellow-400">
                                 <path fillRule="evenodd" d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
                                 <path fillRule="evenodd" d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
                             </svg>
                             </a>
                             <h1 className="text-black text-center">OR</h1>
-                            <a href="" className="bg-yellow-400 text-black py-2 sm:py-4 px-3 sm:px-6 text-center flex rounded-lg items-center justify-between shadow-md josefin text-lg sm:text-xl group ease duration-100 active:bg-black"><span className="group-active:text-yellow-400">Counselling / Corporate training</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 group-active:text-yellow-400">
+                            <a href="" className="bg-yellow-400 text-black py-2 sm:py-4 px-3 sm:px-6 text-center flex rounded-lg items-center justify-between shadow-md josefin text-lg sm:text-xl group ease duration-100 active:bg-black"><span className="group-active:text-yellow-400">Counselling &#47; Corporate training</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 group-active:text-yellow-400">
                                 <path fillRule="evenodd" d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
                                 <path fillRule="evenodd" d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
                             </svg>
