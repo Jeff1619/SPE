7ef2ed30d2b2dc3133fd2a16f8b10e8597d8ac3c
Jeffrin G.V <jef1619@gmail.com>
2024-08-16 15:56:27 +0530

    changed

--- app/coaching_services/page.js
+++ app/coaching_services/page.js
@@ -6,6 +6,9 @@ import career from '@/public/coaching/career.png'
 import life from '@/public/coaching/life.png'
 import time from '@/public/coaching/timeAndPro.png'
 import oneOnone from '@/public/coaching/oneOnone.png'
+import phoenix from '@/public/coaching/phoenix.png'
+import age6 from '@/public/coaching/children.jpg'
+import Icon19 from '@/public/coaching/19icon.png'
 import { motion } from "framer-motion"
 
 
@@ -26,7 +29,7 @@ export default function Courses() {
                         </div>
                     </div>
                 </div>
-                <div>
+                <div className="mb-10 md:mb-12">
                     <h1 className="text-center text-yellow-400 py-3 md:py-5 font-semibold text-xs uppercase md:text-sm">&#45; Our Coaching &#45;</h1>
                     <div className="container mx-auto flex justify-center">
                         <div className="space-y-7 md:flex md:flex-wrap md:gap-10 md:mx-10 md:justify-center md:space-y-0">
@@ -61,9 +64,92 @@ export default function Courses() {
                         </div>
                     </div>
                 </div>
+
+            </div>
+
+            <div className="conatiner mx-auto ">
+                <div className="bg-yellow-400 overflow-hidden">
+                    <svg className="rotate-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"><path fill="#101820ff" fill-opacity="1" d="M500.2,94.7L0,0v100h1000V0L500.2,94.7z"></path></svg>
+                    <div className="py-5">
+                        <h1 className="text-black px-4 text-center font-semibold  md:text-lg space-x-1 md:space-x-4"><span className="great text-3xl md:text-5xl tracking-widest">Guidance</span> <span>and</span> <span className="great text-3xl md:text-5xl tracking-widest">Coaching</span></h1>
+                        <h1 className="text-black font-semibold text-center md:text-lg">Services</h1>
+                        <div className="mt-4 md:mt-5 lg:mt-8">
+                            <p className="px-4 md:px-0 text-sm md:text-base text-justify md:text-center md:w-1/2 md:mx-auto text-black">
+                                At <span className="italic font-sans font-medium">Surging Phoenix Enterprises</span>&#44; we understand that guidance needs to be age&#45;appropriate and developmentally relevant&#46; Our coaching services are designed to support individuals at every stage of life&#44; from childhood through adulthood&#46; Here&#39;s how we cater to different age groups&#46;
+
+                            </p>
+                            <Image src={phoenix} className="mx-auto w-14 mt-3" alt="#" />
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="md:bg-cover md:bg-left-top bg-opacity-90 bg-fixed bg-[url('../public/coaching/age6grnd.png')]">
+                <svg className="rotate-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"><path fill="#facc15" fill-opacity="1" d="M500.2,94.7L0,0v100h1000V0L500.2,94.7z"></path></svg>
+                <div className="container mx-auto">
+                    <div className="py-4">
+                        <h1 className="text-center font-sans font-medium text-xs md:text-lg text-yellow-100">Guidance for <span className="great text-3xl md:text-5xl lg:text-6xl tracking-widest  font-bold bg-clip-text text-transparent bg-gradient-to-r from-yellow-100 to-yellow-400">Children</span></h1>
+                        <h1 className="text-center font-sans font-medium text-yellow-100">&#40;Ages 6-12&#41;</h1>
+                        <p className="my-6 md:my-10 text-justify px-5 md:px-0 md:w-2/3 md:text-center md:mx-auto text-yellow-50">Social Skills Development: Teach children essential social skills, such as communication, empathy, and teamwork, to help them build positive relationships with peers and adults.
+                            Confidence Building: Provide activities and exercises to boost self-esteem and encourage a positive self-image.
+                            Goal Setting and Achievement: Introduce age-appropriate goal-setting techniques and help children develop a sense of accomplishment through small, manageable objectives.
+                            Study Skills and Organization: Help children develop effective study habits, organizational skills, and time management strategies to support their academic success.
+                            Emotional Regulation: Teach children strategies for managing emotions and dealing with stress in a healthy way.
+                        </p>
+                        <Image src={age6} className="w-60 md:h-60 lg:h-80 object-cover object-top md:w-1/2  mx-auto" alt="#" />
+                    </div>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"><path fill="#facc15" fill-opacity="1" d="M500.2,94.7L0,0v100h1000V0L500.2,94.7z"></path></svg>
             </div>
 
 
+            <div className="bg-cover  bg-fixed bg-no-repeat bg-[url('../public/coaching/age19backgrnd.png')]">
+                <div className="container mx-auto">
+                    <div className="py-4 md:py-9 px-5">
+                        <h1 className="text-center font-sans font-medium text-xs md:text-lg text-black">Guidance for
+                            <span className="great text-3xl md:text-5xl lg:text-6xl tracking-widest  font-bold  text-black">Teens</span></h1>
+                        <h1 className="text-center font-sans font-medium text-black">&#40;Ages 13-19&#41;</h1>
+                        <div className="space-y-4 md:space-y-8 md:my-10">
+                            <div >
+                                <h1 className="text-black md:text-center caveat underline underline-offset-4 text-2xl md:text-3xl font-semibold py-2">Academic and Career Planning:</h1>
+                                <p className="text-black md:w-1/2 md:mx-auto md:text-center text-sm md:text-base text-pretty">Assist teens in exploring career interests, setting academic goals, and preparing for college or vocational training.
+                                </p>
+                            </div>
+
+                            <div >
+                                <h1 className="text-black md:text-center caveat underline underline-offset-4 text-2xl font-semibold py-2 md:text-3xl">Self-Discovery and Identity:</h1>
+                                <p className="text-black md:w-1/2 md:mx-auto md:text-center text-sm md:text-base text-pretty">Support teens in understanding their values, strengths, and interests as they develop their personal identity.
+
+                                </p>
+                            </div>
+                            <div >
+                                <h1 className="text-black md:text-center caveat underline underline-offset-4 text-2xl font-semibold py-2 md:text-3xl">Stress and Anxiety Management:</h1>
+                                <p className="text-black md:w-1/2 md:mx-auto md:text-center text-sm md:text-base text-pretty">Provide tools and techniques to manage academic pressures, social stressors, and anxiety.
+                                </p>
+                            </div>
+                            <div >
+                                <h1 className="text-black md:text-center caveat underline underline-offset-4 text-2xl font-semibold py-2 md:text-3xl">Healthy Relationships:</h1>
+                                <p className="text-black md:w-1/2 md:mx-auto md:text-center text-sm md:text-base text-pretty">Educate teens about building healthy relationships, effective communication, and resolving conflicts.
+
+                                </p>
+                            </div>
+                            <div >
+                                <h1 className="text-black md:text-center caveat underline underline-offset-4 text-2xl  font-semibold py-2 md:text-3xl">Life Skills Development:</h1>
+                                <p className="text-black md:w-1/2 md:mx-auto md:text-center text-sm md:text-base text-pretty">Focus on practical life skills such as financial literacy, decision-making, and problem-solving.
+
+
+                                </p>
+                            </div>
+                            <Image src={Icon19} className="w-32 mx-auto" alt="#" />
+                        </div>
+                    </div>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"><path fill="#101820ff" fill-opacity="1" d="M500.2,94.7L0,0v100h1000V0L500.2,94.7z"></path></svg>
+            </div>
         </>
     )
 }
+
+// Guidance for Teens (Ages 13-19)
--- components/Header.tsx
+++ components/Header.tsx
@@ -89,7 +89,7 @@ export default function Header({ handleToggle }: { handleToggle: () => void }) {
 
             {/* FLOWTING ICONS */}
             <div>
-                <button onClick={handleToggle} className='sm:hidden active:bg-yellow-500 uppercase flex items-center justify-center text-sm  fixed py-2 z-10 bottom-0 w-full bg-yellow-400 text-black font-medium'>
+                {/* <button onClick={handleToggle} className='sm:hidden active:bg-yellow-500 uppercase flex items-center justify-center text-sm  fixed py-2 z-10 bottom-0 w-full bg-yellow-400 text-black font-medium'>
                     <span className='pr-2'>Apply Now</span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 ">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
@@ -100,8 +100,8 @@ export default function Header({ handleToggle }: { handleToggle: () => void }) {
                         <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
                     </svg>
 
-                </button>
-                <a href="tel:++91-#" className="p-3 sm:p-5 fixed z-10 rounded-full bottom-32 sm:bottom-32 md:bottom-60 lg:bottom-32 right-4  md:right-5 bg-green-100 group active:bg-green-600"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7 sm:size-10 text-green-600 group-active:text-green-100">
+                </button> */}
+                {/* <a href="tel:++91-#" className="p-3 sm:p-5 fixed z-10 rounded-full bottom-32 sm:bottom-32 md:bottom-60 lg:bottom-32 right-4  md:right-5 bg-green-100 group active:bg-green-600"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7 sm:size-10 text-green-600 group-active:text-green-100">
                     <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
                 </svg></a>
                 <a href="https://wa.me/+91#" target='_blank' className="p-3 sm:p-5 fixed z-10 rounded-full bottom-16 sm:bottom-6 md:bottom-32 lg:bottom-8 right-4 md:right-5  bg-green-500 group active:bg-green-50">
@@ -114,7 +114,7 @@ export default function Header({ handleToggle }: { handleToggle: () => void }) {
                                 d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
                         </svg>
                     </span>
-                </a>
+                </a> */}
 
             </div>
 
